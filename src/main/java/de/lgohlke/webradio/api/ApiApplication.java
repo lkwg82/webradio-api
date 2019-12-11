f800eed79168f6b6c8469afe59d6fd43092650ad
Lars K.W. Gohlke <lkwg82@gmx.de>
2019-12-11 10:20:48 +0100

    adds cors headers

--- src/main/java/de/lgohlke/webradio/api/ApiApplication.java
+++ src/main/java/de/lgohlke/webradio/api/ApiApplication.java
@@ -2,12 +2,25 @@ package de.lgohlke.webradio.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
+    @Bean
+    WebMvcConfigurer mvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://lkwg82.github.io/webradio");
+            }
+        };
+    }
 }
