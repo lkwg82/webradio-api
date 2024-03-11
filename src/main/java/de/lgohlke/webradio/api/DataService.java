87957ad398bb25f3feacba57eb606fa10a41eb84
Lars K.W. Gohlke <lkwg82@gmx.de>
2024-03-11 20:09:09 +0100

    fixes changed response media type

--- src/main/java/de/lgohlke/webradio/api/DataService.java
+++ src/main/java/de/lgohlke/webradio/api/DataService.java
@@ -1,18 +1,25 @@
 package de.lgohlke.webradio.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.lgohlke.webradio.api.data.NowPlaying;
 import de.lgohlke.webradio.api.data.Result;
 import de.lgohlke.webradio.api.data.StationInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.InvalidMediaTypeException;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
@@ -25,6 +32,35 @@ class DataService {
                 .rootUri(uri)
                 .defaultHeader(HttpHeaders.USER_AGENT, "radio.de")
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .additionalMessageConverters(new HttpMessageConverter() {
+                    @Override
+                    public List<MediaType> getSupportedMediaTypes() {
+                        return List.of(MediaType.TEXT_PLAIN);
+                    }
+
+                    @Override
+                    public Object read(Class clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+                        var bytes = inputMessage.getBody()
+                                                .readAllBytes();
+                        var objectMapper = new ObjectMapper();
+                        return objectMapper.readValue(bytes, clazz);
+                    }
+
+                    @Override
+                    public void write(Object o, MediaType contentType, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+
+                    }
+
+                    @Override
+                    public boolean canWrite(Class clazz, MediaType mediaType) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean canRead(Class clazz, MediaType mediaType) {
+                        return true;
+                    }
+                })
                 .defaultHeader(HttpHeaders.ACCEPT_CHARSET, StandardCharsets.UTF_8.name())
                 .build();
     }
